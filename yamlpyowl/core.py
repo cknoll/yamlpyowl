e53d241a1a7e7be5ded081bcad1035074e6fca48
Carsten Knoll <carsten.knoll@posteo.de>
2020-10-03 15:14:35 +0200

    refactor functions to object orientation

--- yamlpyowl/core.py
+++ yamlpyowl/core.py
@@ -8,129 +8,140 @@ from owlready2 import Thing, FunctionalProperty, Imp, sync_reasoner_pellet, get_
 from ipydex import IPS, activate_ips_on_exception
 activate_ips_on_exception()
 
-name_mapping = {
-    "Thing": Thing,
-    "FunctionalProperty": FunctionalProperty,
-    "SymmetricProperty": SymmetricProperty,
-    "TransitiveProperty": TransitiveProperty,
-}
 
+class Ontology(object):
+    def __init__(self, iri, fpath):
+        self.new_classes = []
+        self.concepts = []
+        self.roles = []
+        self.individuals = []
 
-def get_named_objects_from_sequence(seq):
-    """
+        # "http://onto.ackrep.org/pandemic_rule_ontology.owl"
+        self.onto = get_ontology(iri)
 
-    :param seq: list of strings (coming from an yaml list)
-    :return:    list of matching objects from the `name_mapping`
-    """
+        self.name_mapping = {
+            "Thing": Thing,
+            "FunctionalProperty": FunctionalProperty,
+            "SymmetricProperty": SymmetricProperty,
+            "TransitiveProperty": TransitiveProperty,
+        }
 
-    res = []
-    for elt in seq:
-        # assume elt is a string
-        if elt not in name_mapping:
-            raise ValueError(f"unknown name: {elt}")
-        else:
-            res.append(name_mapping[elt])
-    return res
+        self.load_ontology(fpath)
 
+    def get_named_objects_from_sequence(self, seq):
+        """
 
-def get_named_object(data_dict, key_name):
-    """
+        :param seq: list of strings (coming from an yaml list)
+        :return:    list of matching objects from the `self.name_mapping`
+        """
 
-    :param data_dict:   source dict (part of the parsed yaml data)
-    :param key_name:    name (str) for the desired object
-    :return:            the matching object from `name_mapping`
-    """
+        res = []
+        for elt in seq:
+            # assume elt is a string
+            if elt not in self.name_mapping:
+                raise ValueError(f"unknown name: {elt}")
+            else:
+                res.append(self.name_mapping[elt])
+        return res
 
-    if key_name not in data_dict:
-        return None
+    def get_named_object(self, data_dict, key_name):
+        """
 
-    value_name = data_dict[key_name]
+        :param data_dict:   source dict (part of the parsed yaml data)
+        :param key_name:    name (str) for the desired object
+        :return:            the matching object from `self.name_mapping`
+        """
 
-    if value_name not in name_mapping:
-        raise ValueError(f"unknown name: {value_name} (value for {key_name})")
+        if key_name not in data_dict:
+            return None
 
-    return name_mapping[value_name]
+        value_name = data_dict[key_name]
 
+        if value_name not in self.name_mapping:
+            raise ValueError(f"unknown name: {value_name} (value for {key_name})")
 
-def create_individual(i_name, data_dict):
+        return self.name_mapping[value_name]
 
-    kwargs = {}
+    def create_individual(self, i_name, data_dict):
 
-    isA = get_named_object(data_dict, "isA")
-    for key, value in data_dict.items():
-        if key == "isA":
-            continue
-        property_object = name_mapping.get(key)
-        if not property_object:
-            # key_name was not found
-            continue
-        property_values = get_named_objects_from_sequence(value)
-        kwargs[key] = property_values
+        kwargs = {}
 
-    new_individual = isA(**kwargs)
+        isA = self.get_named_object(data_dict, "isA")
+        for key, value in data_dict.items():
+            if key == "isA":
+                continue
+            property_object = self.name_mapping.get(key)
+            if not property_object:
+                # key_name was not found
+                continue
+            property_values = self.get_named_objects_from_sequence(value)
+            kwargs[key] = property_values
 
-    return new_individual
+        new_individual = isA(**kwargs)
 
+        return new_individual
 
 # noinspection PyPep8Naming
+    def load_ontology(self, fpath):
+
+        with open(fpath, 'r') as myfile:
+            d = yaml.load(myfile)
+
+        self.new_classes = []
+        self.concepts = []
+        self.roles = []
+        self.individuals = []
+
+        # provide namespace for classes via `with` statement
+        with self.onto:
+            for c_name, data in d["owl_concepts"].items():
+                # owl_concepts is a dict like {'GeographicEntity': {'subClassOf': 'Thing'}, ...}
+                sco = self.get_named_object(data, "subClassOf")
+                if c_name in self.name_mapping:
+                    msg = f"This concept name was declared more than once: {c_name}"
+                    raise ValueError(msg)
+
+                # now define the class
+                new_class = type(c_name, (sco,), {})
+
+                self.name_mapping[c_name] = new_class
+                self.new_classes.append(new_class)
+                self.concepts.append(new_class)
+
+            for r_name, data in d["owl_roles"].items():
+                # owl_roles: dict like {'hasDirective': [{'mapsFrom': 'GeographicEntity'}, {'mapsTo': 'Directive'}]}
+                try:
+                    mapsFrom = self.name_mapping[data.get("mapsFrom")]
+                    mapsTo = self.name_mapping[data.get("mapsTo")]
+                except KeyError:
+                    msg = f"Unknown concept name for `mapsFrom` or mapsTo in : {r_name}"
+                    raise ValueError(msg)
+                if r_name in self.name_mapping:
+                    msg = f"This name was declared more than once: {r_name}"
+                    raise ValueError(msg)
+                assert issubclass(mapsFrom, Thing)
+                assert issubclass(mapsTo, Thing)
+                from_to_type = mapsFrom >> mapsTo
+
+                additional_properties = self.get_named_objects_from_sequence(data.get("properties", []))
+                kwargs = {}
+                inverse_property = self.get_named_object(data, "inverse_property")
+                if inverse_property:
+                    kwargs["inverse_property"] = inverse_property
+
+                new_class = type(r_name, (from_to_type, *additional_properties), kwargs)
+                self.name_mapping[r_name] = new_class
+                self.new_classes.append(new_class)
+                self.roles.append(new_class)
+
+            for i_name, data in d["owl_individuals"].items():
+                new_individual = self.create_individual(i_name, data)
+
+                self.individuals.append(new_individual)
+                self.name_mapping[i_name] = new_individual
+
+
 def main(fpath):
+    o = Ontology(iri="test", fpath=fpath)
+    return o
 
-    with open(fpath, 'r') as myfile:
-        d = yaml.load(myfile)
-
-    onto = get_ontology("http://onto.ackrep.org/pandemic_rule_ontology.owl")
-
-    new_classes = []
-    concepts = []
-    roles = []
-    individuals = []
-
-    # provide namespace for classes via `with` statement
-    with onto:
-        for c_name, data in d["owl_concepts"].items():
-            # owl_concepts is a dict like {'GeographicEntity': {'subClassOf': 'Thing'}, ...}
-            sco = get_named_object(data, "subClassOf")
-            if c_name in name_mapping:
-                msg = f"This concept name was declared more than once: {c_name}"
-                raise ValueError(msg)
-
-            # now define the class
-            new_class = type(c_name, (sco,), {})
-
-            name_mapping[c_name] = new_class
-            new_classes.append(new_class)
-            concepts.append(new_class)
-
-        for r_name, data in d["owl_roles"].items():
-            # owl_roles: dict like {'hasDirective': [{'mapsFrom': 'GeographicEntity'}, {'mapsTo': 'Directive'}]}
-            try:
-                mapsFrom = name_mapping[data.get("mapsFrom")]
-                mapsTo = name_mapping[data.get("mapsTo")]
-            except KeyError:
-                msg = f"Unknown concept name for `mapsFrom` or mapsTo in : {r_name}"
-                raise ValueError(msg)
-            if r_name in name_mapping:
-                msg = f"This name was declared more than once: {r_name}"
-                raise ValueError(msg)
-            assert issubclass(mapsFrom, Thing)
-            assert issubclass(mapsTo, Thing)
-            from_to_type = mapsFrom >> mapsTo
-
-            additional_properties = get_named_objects_from_sequence(data.get("properties", []))
-            kwargs = {}
-            inverse_property = get_named_object(data, "inverse_property")
-            if inverse_property:
-                kwargs["inverse_property"] = inverse_property
-
-            new_class = type(r_name, (from_to_type, *additional_properties), kwargs)
-            name_mapping[r_name] = new_class
-            new_classes.append(new_class)
-            roles.append(new_class)
-
-        for i_name, data in d["owl_individuals"].items():
-            new_individual = create_individual(i_name, data)
-
-            individuals.append(new_individual)
-            name_mapping[i_name] = new_individual
-
-    IPS()
