87c96d00ecd46876792dc1fb460e7d582b3bab79
Carsten Knoll <carsten.knoll@posteo.de>
2021-02-12 18:35:37 +0100

    add LICENSE and fix setup.py

--- setup.py
+++ setup.py
@@ -7,11 +7,16 @@ import setuptools
 from setuptools import setup, find_packages
 
 
-packagename = 'yamlpyowl'
+packagename = "yamlpyowl"
 
 # consider the path of `setup.py` as root directory:
 PROJECTROOT = os.path.dirname(sys.argv[0]) or "."
-__version__ = open(os.path.join(PROJECTROOT, "src", packagename, "release.py")).read().split('__version__ = "', 1)[1].split('"', 1)[0]
+__version__ = (
+    open(os.path.join(PROJECTROOT, "src", packagename, "release.py"), encoding="utf8")
+    .read()
+    .split('__version__ = "', 1)[1]
+    .split('"', 1)[0]
+)
 
 
 with open("requirements.txt") as requirements_file:
@@ -20,22 +25,21 @@ with open("requirements.txt") as requirements_file:
 setup(
     name=packagename,
     version=__version__,
-    author='Carsten Knoll',
-    author_email='Carsten.Knoll@tu-dresden.de',
+    author="Carsten Knoll",
+    author_email="Carsten.Knoll@tu-dresden.de",
     packages=find_packages("src"),
-    package_dir={'': 'src'},
-    url='https://github.com/cknollyamlpyowl',
-    license='GPLv3',
-    description='...',
+    package_dir={"": "src"},
+    url="https://github.com/cknoll/yamlpyowl",
+    license="GPLv3",
+    description="...",
     long_description="""
     ...
     """,
     install_requires=requirements,
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python :: 3',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3",
     ],
 )
-
